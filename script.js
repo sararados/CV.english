7e6cbaf2bb8e8bc3f70451433c6f3e8d2f402354
Sara Rados <sararados@hotmail.com>
2023-04-18 13:50:34 +0200

    cv nästan klart, måste snygga till pdf

--- script.js
+++ script.js
@@ -44,3 +44,70 @@ function scrollHeader() {
     else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    
+    if(this.scrollY >= 500) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+
+//Dark Light Theme
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+//validate if user chose a topic
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+function scaleCv(){
+    document.body.classList.add('scale-cv');
+}
+
+function removeScale() {
+    document.body.classList.remove('scale-cv');
+}
+
+let areaCv = document.getElementById('area-cv')
+
+let resumeButton = document.getElementById('resume-button');
+
+
+let opt = {
+  margin:       0,
+  filename:     'myresume.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 4 },
+  jsPDF:        { format: 'A4', orientation: 'portrait' }
+};
+
+
+function generateResume() {
+    html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener('click', () => {
+    scaleCv();
+    generateResume();
+
+    setTimeout(removeScale, 5000)
+})
